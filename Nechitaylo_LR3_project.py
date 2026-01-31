5da521e959798b334cc2f82864ff9cdf358dc7ed
александр <yashin.vain@mail.ru>
2026-01-31 20:22:18 +0300

    Исправление ошибок

--- Nechitaylo_LR3_project.py
+++ Nechitaylo_LR3_project.py
@@ -6,16 +6,20 @@ def Y():
 def Z(): 
     z = float(input(f"\nВведите вещественное число z:  "))
 
+
 def SUMO(x, y, z):
     frac_x = x - int(x)
     frac_y = y - int(y)
     frac_z = z - int(z)
     print(frac_x + frac_y + frac_z)
+
 def CEL_SUMO(x, y, z):
     int_x = int(x)
     int_y = int(y)
     int_z = int(z)
     print(int_x + int_y + int_z)
+
+x, y, z = None, None, None
 
 while True:
     choice = int(input(f"\n1 - Ввести вещественное число x\n2 - Ввести вещественное число y" \
@@ -29,6 +33,12 @@ while True:
     elif choice == 3:
         z = Z()
     elif choice == 4:
-        sumo = SUMO(x, y, z)
+        if x is None or y is None or z is None:
+            print(f"\nВнимание! Не все числа введены!")
+        else:
+            sumo = SUMO(x, y, z)
     elif choice == 5:
-        cel_sumo = CEL_SUMO(x, y, z)
+        if x is None or y is None or z is None:
+            print(f"Внимание! Не все числа введены!")
+        else:
+            cel_sumo = CEL_SUMO(x, y, z)
